a119cc3e09c78692d2014030eeb9eb43d1cd91bf
Guna P <gunaps810@gmail.com>
2022-11-09 14:02:22 +0530

    aiosqlite added to requirements

--- main.py
+++ main.py
@@ -119,7 +119,6 @@ async def get_addresses():
 
 # distance calculation function
 def distance_between_two_points(lat1, lon1, lat2, lon2):
-    print(lat1, lon1, lat2, lon2)
     p = 0.017453292519943295
     a = 0.5 - cos((lat2 - lat1) * p) / 2 + cos(lat1 * p) * cos(lat2 * p) * (1 - cos((lon2 - lon1) * p)) / 2
     return 12742 * asin(a)
